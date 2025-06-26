6b8207079d66e31517384e8a508e927e6022219b
mlengineer007 <rashnik.2020@gmail.com>
2025-06-26 16:24:59 -0500

    added port

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import boto3
 import csv
 import io
 from datetime import datetime
+import os 
 
 app = Flask(__name__)
 
@@ -45,4 +46,5 @@ def index():
         return f"Error fetching jobs: {str(e)}"
 
 if __name__ == '__main__':
+    port = int (os.environ.get("PORT",5000))
     app.run(host = '0.0.0.0',debug=True)
