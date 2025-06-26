0ccdb9af8c8fa520a3df8fc384b48d08c9fa9f31
mlengineer007 <rashnik.2020@gmail.com>
2025-06-26 15:28:34 -0500

    code change A

--- Read_API.py
+++ Read_API.py
@@ -9,7 +9,7 @@ rapidapikey = os.getenv('rapidapikey')
 conn = http.client.HTTPSConnection("linkedin-data-scraper-api1.p.rapidapi.com")
 
 headers = {
-    'x-rapidapi-key':  rapidapikey ,
+    'x-rapidapi-key':  'acdfb5f277msh84114c407295bf7p170cf8jsn5dff8b03ec3d' ,
     'x-rapidapi-host': "linkedin-data-scraper-api1.p.rapidapi.com"
 }
 
